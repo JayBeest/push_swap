fce3cf1929ecb6fab5e1f3df4ddb85192d6c7106
jcorneli <jaybart@protonmail.com>
2021-09-27 10:16:05 +0200

    to school

--- src/incl/main.h
+++ src/incl/main.h
@@ -7,5 +7,28 @@ typedef enum e_bool
 	TRUE
 }			t_bool;
 
+typedef enum e_err_no
+{
+	NO_ERROR,
+	NO_ARGS,
+	NO_DIGIT,
+	NO_INT,
+	DEFAULT_ERROR
+}			t_err_no;
+
+typedef struct s_bool_err
+{
+	t_bool		bool;
+	t_err_no	error_nr;
+}			t_bool_err;
+
+typedef struct s_ps_stacks
+{
+	int	*stack_a;
+	int	*stack_b;
+	int amount_of_integers;
+	int	stack_a_size;
+	int stack_b_size;
+}			t_ps_stacks;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,10 +3,48 @@
 
 #include <stdio.h>
 
-int parse_arguments(char **argv, int stack_a[])
+void	init_num_to_stack(int num, int *stack, int amount_of_integers)
+{
+	static int i;
+
+	if (i < amount_of_integers)
+	{
+		*(stack + i) = num;
+		i++;
+	}
+	else
+		printf("Too many inits?\n");
+}
+
+int	duplicates(int *stack, int amount_of_integers)
+{
+	int	i;
+	int j;
+	int	duplicates;
+
+	i = 0;
+	duplicates = 0;
+	while (i < amount_of_integers && !duplicates)
+	{
+		j = 0;
+		duplicates = -1;
+		while (j < amount_of_integers)
+		{
+			printf("stack[i]: %d, stack[j]: %d  (i / j pointers: %p / %p)\n", stack[i], stack[j], &stack[i], &stack[j]);
+			if (stack[i] == stack[j])
+				duplicates++;
+			j++;
+		}
+		i++;
+	}
+	return (duplicates);
+}
+
+int parse_arguments(char **argv, t_ps_stacks *stacks)
 {
 	int 		i;
 	long long	num;
+//	t_bool_err 	bool_err;
 
 	argv++;
 	while (*argv)
@@ -23,19 +61,62 @@ int parse_arguments(char **argv, int stack_a[])
 		num = ft_atoi(*argv);
 		if (num > 2147483647 || num < -2147483648)
 			return (printf("ONLY integers (check min/max) :(n"));
-		if (!add_index_to_stack(stack_a, num))
-			return (printf("add_stack error?"));
+		init_num_to_stack(num, stacks->stack_b, stacks->amount_of_integers);
 		argv++;
+		printf("duplicates: %d\n", duplicates(stacks->stack_b, stacks->amount_of_integers));
 	}
 	return (1);
 }
 
+void	nums_to_indexes(t_ps_stacks *stacks)
+{
+	int	i;
+	int	j;
+	int smoller_count;
+
+	i = 0;
+	while (i < stacks->amount_of_integers)
+	{
+		j = 0;
+		smoller_count = 0;
+		while (j < stacks->amount_of_integers)
+		{
+			if (stacks->stack_b[j] < stacks->stack_b[i])
+				smoller_count++;
+			j++;
+		}
+		stacks->stack_a[i] = smoller_count;
+		i++;
+	}
+//	ft_bzero(stacks->stack_b, stacks->amount_of_integers * sizeof(int));
+}
+
+void	print_stack(int *stack, int stack_size)
+{
+	int	i;
+
+	i = 0;
+	while (i < stack_size)
+	{
+		printf("index %d: %d\n", i, stack[i]);
+		i++;
+	}
+
+}
+
 int	main(int argc, char **argv)
 {
+	t_ps_stacks	stacks;
 	int	stack_a[argc - 1];
-//	int stack_b[argc - 1];
+	int	stack_b[argc - 1];
 
-	parse_arguments(argv, stack_a);
-
+	stacks.stack_a = stack_a;
+	stacks.stack_b = stack_b;
+	stacks.amount_of_integers = argc - 1;
+	stacks.stack_a_size = stacks.amount_of_integers;
+	stacks.stack_b_size = 0;
+	parse_arguments(argv, &stacks);
+	nums_to_indexes(&stacks);
+	print_stack(stacks.stack_a, stacks.stack_a_size);
 	return (0);
 }
